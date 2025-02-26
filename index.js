e41babb2cbddb9a6c26e70ca09423c2101fbc3f4
venkatesh <venky@example.com>
2025-02-26 11:56:52 -0800

    index.js made changes

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const vendorRoutes=require('./routes/vendorRoutes')
 const bodyParser=require('body-parser')
 const firmRoutes=require('./routes/firmRoutes')
 const productRoutes=require('./routes/productRoutes')
-const cors=require('cors')
-const path=require('path')
 
 const app=express();
 
-const PORT=4000;
+const PORT=process.env.PORT || 4000;
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("mongodb is connected successfully")
@@ -24,4 +22,8 @@ app.use('/product',productRoutes);
 app.use('/uploads',express.static('uploads'))
 app.listen(PORT,()=>{
     console.log("port is running on port 4000");
+})
+
+app.use('/',(req,res)=>{
+    res.send("<h1>welcome to SUBY</h1>")
 })
