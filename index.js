e07d85b06ef18cda8ab045c226cd37bc39e84611
venkatesh <venky@example.com>
2025-02-26 12:34:40 -0800

    changes made

--- index.js
+++ index.js
@@ -25,5 +25,6 @@ app.listen(PORT,()=>{
 })
 
 app.use('/',(req,res)=>{
-    res.send("<h1>welcome to SUBY</h1>")
+    res.send("<h1>hi Venkatesh</h1>")
+    res.send("<h1>welcome to SUBY </h1>")
 })
